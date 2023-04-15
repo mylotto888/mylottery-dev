9cc923bb02a0ea8c9de13cd818e5ade1d2ec58ca
tunmin11 <tunmin2000@gmail.com>
2023-04-15 17:21:59 +0630

    Added Mint Box

--- src/components/MintBox/index.jsx
+++ src/components/MintBox/index.jsx
@@ -1,24 +1,27 @@
 const MintBox = () => {
 
     return (
-        <div className="text-gray-900 h-4/5 w-full flex justify-center">
-            <div className="card w-96 rounded-md bg-gray-700">
+        <div className="text-gray-900 mt-12 w-full flex justify-center">
+            <div className="card w-96 rounded-md bg-white shadow-lg">
                 <figure className="flex items-center justify-center h-full py-10 border-2 border-red-500 bg-white">
                     <img src="/logo192.png" className="boarder border-red-500 w-3/5" alt="MyLotte" />
                 </figure>
-                <div className="card-body">
-                    <div>
-                        <div>
-                            <span>Remaining</span>
-                            <span>568</span>
+                <div className="card-body flex justify-center gap-8">
+                    <div className="flex px-5 gap-10">
+                        <div className="flex flex-col tracking-wider gap-1">
+                            <span className="text-gray-400">Remaining</span>
+                            <span className="">568</span>
                         </div>
-                        <div>
-                            <span>Price</span>
+                        <div className="flex flex-col tracking-wider gap-1">
+                            <span className="text-gray-400">Price</span>
                             <span>🍀 0.01</span>
                         </div>
-                        <div>
-                            <button>live</button>
+                        <div className="py-2 flex item-center">
+                            <button className="btn btn-sm text-red-500 bg-gray-300 border-none hover:text-red-600 hover:bg-gray-300">live</button>
                         </div>
+                    </div>
+                    <div className="w-full flex justify-center">
+                        <button className="btn btn-block bg-red-500 hover:bg-red-600 border-none">mint</button>
                     </div>
                 </div>
             </div>
